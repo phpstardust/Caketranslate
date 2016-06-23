b515e550c79091b4f11e09cd07a2116a85b00e64
PhpStarDust <info@phpstardust.org>
2016-06-23 17:58:51 +0200

    Create CaketranslateController.php

---
+++ Controller/CaketranslateController.php
@@ -0,0 +1,35 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class CaketranslateController extends AppController {
+	
+	public $uses = array();
+	
+	public $components = array('Session','Cookie','RequestHandler');
+	
+	public $helpers = array('Html', 'Form', 'Session');
+	
+	
+	
+	
+	public function setlang($lang = NULL) {
+		
+		if ($lang!==NULL) {
+		
+			(string)$language = trim(strip_tags($lang));
+			
+			Configure::write('Config.language', $language);
+			$this->Session->write('Config.language', $language);
+			CakeSession::write('Config.language', $language);
+			
+			$this->redirect($this->referer());
+		
+		}
+		
+    }
+	
+	
+	
+}
+?>
